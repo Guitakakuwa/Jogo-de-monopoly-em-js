87c8101985fe653648d764b58f63646ef76ab5d0
Rodrigo Ceccato de Freitas <rodrigoceccatodefreitas@gmail.com>
2018-01-17 16:55:29 -0200

    added gameLogic and PLAYER

---
+++ js/gameLogic.js
@@ -0,0 +1,105 @@
+// player class
+function Player (name, id) {
+    this.name = name;
+    this.money = 0;
+    this.inPrision = false;
+    this.prisionTime = 0;
+    this.position = 0;
+    this.playerID = id;
+    this.belongings = [];
+
+    this.getName = function() {
+        return this.name;
+    };
+
+    this.getID = function(){
+        return this.playerID;
+    };
+
+    this.setPosition = function(newPosition){
+        this.position = newPosition;
+    };
+
+    this.getPosition = function(){
+        return this.position;
+    };
+
+    this.pay = function(value){
+        //  sucess payment
+        if(this.money >= value) {
+            this.money -= value;
+            return true;
+        }
+
+        // payment failiure
+        else {
+            return false;
+        }
+    };
+
+    this.reciveMoney = function(value){
+        this.money += value;
+    };
+
+    this.addProperty = function(newLand){
+        (this.belongings).push(newLand);
+    };
+
+    this.getProperty = function(){
+        return this.belongings;
+    };
+
+    this.getMoney = function(){
+        return this.money;
+    };
+
+    this.isInPrision = function(){
+        return this.inPrision;
+    };
+
+    this.getJailTime = function(){
+        return this.prisionTime;
+    };
+
+    this.goToJail = function(){
+        this.prisionTime = 3;
+        this.inPrision = true;
+    };
+
+    this.getFree = function(){
+        this.prisionTime = 0;
+        this.inPrision = false;
+    };
+
+}
+
+// adds a player, give him or her the starting money
+// and puts it on the playersVetor
+function createPlayer(name){
+    newPlayer = new Player(name, playersVetor.length);
+    newPlayer.reciveMoney(startingMoney);
+
+    (playersVetor).push(newPlayer);
+}
+
+function gameStart(){
+    createPlayer("Ronaldo");
+    createPlayer("Geraldinho");
+    createPlayer("Jamires");
+
+}
+
+function turnLogic(){
+    
+
+    turnNumber++;
+}
+
+//  beggining of the execution
+let playersVetor = [];
+let gameRunning = true;
+let turnNumber = 0;
+const startingMoney = 3500;
+
+// starts the game
+gameStart();
---
+++ js/gameLogicTest.js
@@ -0,0 +1,39 @@
+// console.log("gameLogicTest started!");
+//
+// jog = new Player("Ronaldo");
+//
+// jog.addProperty("brás");
+// jog.addProperty("bexiga");
+// jog.addProperty("Barra funda");
+// // console.log(jog.belongings[0]);
+//
+// let lista = jog.getProperty();
+//
+// // console.log(lista[0]);
+//
+// for(let i = 0; i < lista.length; i++) {
+//     console.log(lista[i]);
+// }
+
+function logPlayers(){
+    let j = 0;
+    let auxList;
+    for(let i = 0; i < playersVetor.length; i++) {
+        console.log("Name: " + playersVetor[i].getName());
+        console.log("ID: " + playersVetor[i].getID());
+        console.log("Money: " + playersVetor[i].getMoney());
+        console.log("Position: " + playersVetor[i].getPosition());
+        console.log("InPrision: (" + playersVetor[i].getJailTime() + "): " + playersVetor[i].isInPrision());
+
+        //  prints all properties of the player
+        auxList = playersVetor[i].getProperty();
+        for(j = 0; j < auxList.length; j++){
+            console.log("  >" + auxList[j].name);
+        }
+
+        console.log(" ");
+        console.log(" ");
+    }
+}
+
+logPlayers();
