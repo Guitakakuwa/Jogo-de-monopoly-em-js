9f31cf770277df58d62592e998aa2bf43250020b
Guitakuwa <guilherme.takakuwa@gmail.com>
2018-01-18 10:18:30 -0200

    adicinado logica nos dados e atualização de layout

--- js/dicelogic.js
+++ js/dicelogic.js
@@ -1,6 +1,35 @@
-
-
-function Aletarios(min,max) {
-	
-	
+let dice1 = {
+    sides: 6,
+    roll: function() {
+        let randomNumber = Math.floor(Math.random() * this.sides) + 1;
+        console.log();randomNumber;
+        return randomNumber;
+    }
 }
+let dice2 = {
+    sides2: 6,
+    roll2: function() {
+        let randomNumber2 = Math.floor(Math.random() * this.sides2) + 1;
+        console.log();randomNumber2;
+        return randomNumber2;
+    }
+}
+function printNumber(number) {
+    let placeholder = document.getElementById('placeholder');
+    placeholder.innerHTML = number;
+	console.log(number);
+}
+function printNumber2(number2) {
+    let placeholder2 = document.getElementById('placeholder2');
+    placeholder2.innerHTML = number2;
+    console.log(number2);
+}
+
+let button = document.getElementById('button');
+
+button.onclick = function() {
+    let result1 = dice1.roll();
+    let result2 = dice2.roll2();
+    printNumber2(result2);
+    printNumber(result1);
+};
