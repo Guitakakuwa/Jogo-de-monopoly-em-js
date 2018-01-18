d2f63b9b737cbf284199734cc827cc502a02edae
Guitakuwa <guilherme.takakuwa@gmail.com>
2018-01-18 15:19:38 -0200

    refresh

---
+++ js/show_position_money.js
@@ -0,0 +1,9 @@
+function printPosition(position_show) {
+    let position_show = document.getElementById('position_show');
+    position_show.innerHTML = position_show;
+}
+
+function printMoney(money_show) {
+    let money_show= document.getElementById('money_show');
+    money_show.innerHTML = money_show;
+}
