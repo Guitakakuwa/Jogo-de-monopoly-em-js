86ca63db8ef4caa29f227d3717075b5a64d1e70f
Guitakuwa <guilherme.takakuwa@gmail.com>
2018-01-18 12:03:09 -0200

    help

---
+++ js/cards_logic.js
@@ -0,0 +1,42 @@
+let cards=[35];
+
+
+ card_structure(card_name, card_recive, card_pay,card_description) {
+	card_name:this.card_name;
+	card_recive:this.card_recive;
+	card_pay:this.card_pay;
+	card_description:this.card_description;
+}
+
+
+let card1(Revés, 0, 0, );
+let card2(Revés, 0, 0, );
+let card3(Revés, 0, 0, );
+let card4(Revés, 0, 0, );
+let card5(Revés, 0, 0, );
+let card6(Revés, 0, 0, );
+let card7(Revés, 0, 0, );
+let card8(Revés, 0, 0, );
+let card9(Revés, 0, 0, );
+let card10(Revés, 0, 0, );
+let card11(Revés, 0, 0, );
+let card12(Revés, 0, 0, );
+let card13(Revés, 0, 0, );
+let card14(Revés, 0, 0, );
+let card15(Revés, 0, 0, );
+let card16(Revés, 0, 0, );
+let card17(sorte, 0, 0, );
+let card18(sorte, 0, 0, );
+let card20(sorte, 0, 0, );
+let card21(sorte, 0, 0, );
+let card22(sorte, 0, 0, );
+let card23(sorte, 0, 0, );
+let card24(sorte, 0, 0, );
+let card25(sorte, 0, 0, );
+let card26(sorte, 0, 0, );
+let card27(sorte, 0, 0, );
+let card28(sorte, 0, 0, );
+let card29(sorte, 0, 0, );
+let card30(sorte, 0, 0, );
+let card31(sorte, 0, 0, );
+let card32(sorte, 0, 0, );
